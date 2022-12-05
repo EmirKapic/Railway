0f1b4c0673eca75269c1f9c81e7bfb6463222209
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-05 23:10:35 +0100

    created train-stations

---
+++ src/main/java/ba/unsa/etf/rpr/TrainStations.java
@@ -0,0 +1,50 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class TrainStations {
+    private int trainStationID;
+    private String location;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainStations that = (TrainStations) o;
+        return trainStationID == that.trainStationID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trainStationID, location);
+    }
+
+    @Override
+    public String toString() {
+        return "TrainStations{" +
+                "trainStationID=" + trainStationID +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+    public int getTrainStationID() {
+        return trainStationID;
+    }
+
+    public void setTrainStationID(int trainStationID) {
+        this.trainStationID = trainStationID;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public TrainStations(int trainStationID, String location) {
+        this.trainStationID = trainStationID;
+        this.location = location;
+    }
+}
