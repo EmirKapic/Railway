9e3a96e1a07138ec2d2c1d9ad3f52e1eb2cad9e3
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-05 23:15:26 +0100

    created passengers class

---
+++ src/main/java/ba/unsa/etf/rpr/Passengers.java
@@ -0,0 +1,72 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class Passengers {
+    private String name;
+    private String surname;
+    private int PassengerID;
+    private int TicketID;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passengers that = (Passengers) o;
+        return PassengerID == that.PassengerID;
+    }
+
+    @Override
+    public String toString() {
+        return "Passengers{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", PassengerID=" + PassengerID +
+                ", TicketID=" + TicketID +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, PassengerID, TicketID);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getPassengerID() {
+        return PassengerID;
+    }
+
+    public void setPassengerID(int passengerID) {
+        PassengerID = passengerID;
+    }
+
+    public int getTicketID() {
+        return TicketID;
+    }
+
+    public void setTicketID(int ticketID) {
+        TicketID = ticketID;
+    }
+
+    public Passengers(String name, String surname, int passengerID, int ticketID) {
+        this.name = name;
+        this.surname = surname;
+        PassengerID = passengerID;
+        TicketID = ticketID;
+    }
+}
