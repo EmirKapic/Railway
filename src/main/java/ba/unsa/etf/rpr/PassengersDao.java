49b8ac45d84c2de669049a4185d4c18247f3b720
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-06 21:34:42 +0100

    Created PassengersDAO

---
+++ src/main/java/ba/unsa/etf/rpr/PassengersDao.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+import java.util.List;
+
+public interface PassengersDao extends Dao<Passengers>{
+    //public List<Passengers> searchByName(String name); SHOULD NOT BE USED! People can have the same name which can easily create problems. Use ID only
+}
