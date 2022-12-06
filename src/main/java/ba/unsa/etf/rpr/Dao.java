0e8c6539c07e4d168b6ff3fcbc4a83719c077049
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-06 21:13:49 +0100

    created main DAO interface to be further implemented by specific ones

---
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr;
+
+import java.util.List;
+
+/**
+ * Interface which is to be implemented by all specific-dao classes (departures, passengers etc.)
+ */
+public interface Dao<T> {
+    /**
+     * get object with the specified ID(PK) from database
+     * @param id primary key in the DB
+     * @return Object with the primaryKey
+     */
+    T getById(int id);
+
+    /**
+     * Adds an object to the DB
+     * @param item the object to be added
+     * @return added item
+     */
+    T add(T item);
+
+    /**
+     * Updates an object in the Database
+     * @param item to be updated
+     * @return updated object
+     */
+    T update(T item);
+
+    /**
+     * Deletes the specified object from the DB
+     * @param item object to be deleted
+     * @return Deleted object
+     */
+    T delete(T item);
+
+    /**
+     * Returns all objects from the DB
+     * @return List of all objects
+     */
+    List<T> getAll();
+}
