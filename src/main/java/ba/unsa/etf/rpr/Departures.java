197a0696893e8c83337a7b8a133431bbed2ed729
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-07 18:29:55 +0100

    Changed Departures to use LocalDateTime instead of Date

--- src/main/java/ba/unsa/etf/rpr/Departures.java
+++ src/main/java/ba/unsa/etf/rpr/Departures.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
 
@@ -24,7 +25,7 @@ public class Departures {
         this.endTime = endTime;
     }
 
-    public Departures(int departureID, Date startDate, Date endDate, String length, int ticketsLeft, int startStationID, int endStationID, int ticketsTotal, String startTime, String endTime) {
+    public Departures(int departureID, LocalDateTime startDate, LocalDateTime endDate, String length, int ticketsLeft, int startStationID, int endStationID, int ticketsTotal, String startTime, String endTime) {
         this.departureID = departureID;
         this.startDate = startDate;
         this.endDate = endDate;
@@ -45,11 +46,11 @@ public class Departures {
         this.departureID = departureID;
     }
 
-    public Date getStartDate() {
+    public LocalDateTime getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(Date startDate) {
+    public void setStartDate(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 
@@ -67,11 +68,11 @@ public class Departures {
                 '}';
     }
 
-    public Date getEndDate() {
+    public LocalDateTime getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(Date endDate) {
+    public void setEndDate(LocalDateTime endDate) {
         this.endDate = endDate;
     }
 
@@ -116,8 +117,8 @@ public class Departures {
     }
 
     private int departureID;
-    private Date startDate;
-    private Date endDate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private String length;
     private int ticketsLeft;
     private int startStationID;
