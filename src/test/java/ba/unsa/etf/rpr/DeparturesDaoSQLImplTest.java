c613f82bb666682e564295047002ef03db196502
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-07 20:59:52 +0100

    Unit tests for all relevant methods finished

--- src/test/java/ba/unsa/etf/rpr/DeparturesDaoSQLImplTest.java
+++ src/test/java/ba/unsa/etf/rpr/DeparturesDaoSQLImplTest.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeparturesDaoSQLImplTest {
@@ -12,7 +14,7 @@ class DeparturesDaoSQLImplTest {
         Departures dep = new Departures();
         dep = d.getById(2);
         assertEquals(dep.getDepartureID(), 2);
-        assertEquals(dep.getStartDate().toString(), "2023-01-21");
+        assertEquals(dep.getStartDate().toString(), "2023-01-21T15:00");
         assertEquals(dep.getEndStationID(), 3);
         assertEquals(dep.getLength(), "02:30:00");
         assertEquals(dep.getStartTime(), "15:00");
@@ -27,26 +29,61 @@ class DeparturesDaoSQLImplTest {
     }
 
     @Test
-    void add() {
+    void addDeletegetAlltest() {
+        //we get all at start, add one object, remove one, and get all again, and should be the same as start
+        DeparturesDaoSQLImpl d = new DeparturesDaoSQLImpl();
+        List<Departures> before = d.getAll();
+        Departures dep = new Departures();
+        dep = d.getById(2);
+        d.add(dep); //we add the first row again (to the end, it doesn't have to be the same row im just
+        dep = d.getById(4); //too lazy to write whole new departures object
+        assertEquals(dep.getDepartureID(), 4); //Now we test that the 4th row is indeed there
+        assertEquals(dep.getStartDate().toString(), "2023-01-21T15:00");
+        assertEquals(dep.getEndStationID(), 3);
+        assertEquals(dep.getLength(), "02:30:00");
+        assertEquals(dep.getStartTime(), "15:00");
+        d.delete(dep); //also serves as delete test, table should remain the same
+        List<Departures> after = d.getAll();
+        assertEquals(before,after);
     }
 
     @Test
     void update() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void getAll() {
+        DeparturesDaoSQLImpl d = new DeparturesDaoSQLImpl();
+        Departures dep = new Departures();
+        dep = d.getById(2);
+        int temp = dep.getTicketsLeft(); //We save it so we can return it later
+        dep.setTicketsLeft(0);
+        d.update(dep);
+        dep = d.getById(2);
+        assertEquals(dep.getTicketsLeft(), 0);
+        dep.setTicketsLeft(temp);
+        d.update(dep); //we return the DB to what it was before
     }
 
     @Test
     void searchByStartDate() {
+        DeparturesDaoSQLImpl d = new DeparturesDaoSQLImpl();
+        Departures dep = new Departures();
+        dep = d.getById(1);
+        List<Departures> list = d.searchByStartDate(dep.getStartDate());
+        assertEquals(list.get(0) ,d.getById(1));
+        assertEquals(list.size(), 1);
     }
+
+
 
     @Test
     void searchByStation() {
+        DeparturesDaoSQLImpl d = new DeparturesDaoSQLImpl();
+        List<Departures> list = d.searchByStation("Sarajevo");
+        assertEquals(list.get(0) ,d.getById(1));
+        assertEquals(list.get(1), d.getById(2));
+    }
+    @Test
+    void searchByStation2(){
+        DeparturesDaoSQLImpl d = new DeparturesDaoSQLImpl();
+        List<Departures> list = d.searchByStation("Cazin");
+        assertEquals(list.size(), 0); //Nema pruge u cazinu :(
     }
 }
