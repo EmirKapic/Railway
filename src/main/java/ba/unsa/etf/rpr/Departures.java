d0e1a227696f8d32a5139531edd266ce76e8334c
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-07 17:12:31 +0100

    update

--- src/main/java/ba/unsa/etf/rpr/Departures.java
+++ src/main/java/ba/unsa/etf/rpr/Departures.java
@@ -8,23 +8,41 @@ public class Departures {
     public Departures() {
     }
 
-    public Departures(int departureID, Date startDate, Date endDate, String length, int ticketsLeft, TrainStations startStation, TrainStations endStation, int ticketsTotal) {
-        DepartureID = departureID;
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public Departures(int departureID, Date startDate, Date endDate, String length, int ticketsLeft, int startStationID, int endStationID, int ticketsTotal, String startTime, String endTime) {
+        this.departureID = departureID;
         this.startDate = startDate;
         this.endDate = endDate;
         this.length = length;
         this.ticketsLeft = ticketsLeft;
-        this.startStation = startStation;
-        this.endStation = endStation;
+        this.startStationID = startStationID;
+        this.endStationID = endStationID;
         this.ticketsTotal = ticketsTotal;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     public int getDepartureID() {
-        return DepartureID;
+        return departureID;
     }
 
     public void setDepartureID(int departureID) {
-        DepartureID = departureID;
+        this.departureID = departureID;
     }
 
     public Date getStartDate() {
@@ -38,13 +56,13 @@ public class Departures {
     @Override
     public String toString() {
         return "Departures{" +
-                "DepartureID=" + DepartureID +
+                "DepartureID=" + departureID +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
                 ", length='" + length + '\'' +
                 ", ticketsLeft=" + ticketsLeft +
-                ", startStation='" + startStation + '\'' +
-                ", endStation='" + endStation + '\'' +
+                ", startStation='" + startStationID + '\'' +
+                ", endStation='" + endStationID + '\'' +
                 ", ticketsTotal=" + ticketsTotal +
                 '}';
     }
@@ -73,20 +91,20 @@ public class Departures {
         this.ticketsLeft = ticketsLeft;
     }
 
-    public TrainStations getStartStation() {
-        return startStation;
+    public int getStartStationID() {
+        return startStationID;
     }
 
-    public void setStartStation(TrainStations startStation) {
-        this.startStation = startStation;
+    public void setStartStationID(int startStationID) {
+        this.startStationID = startStationID;
     }
 
-    public TrainStations getEndStation() {
-        return endStation;
+    public int getEndStationID() {
+        return endStationID;
     }
 
-    public void setEndStation(TrainStations endStation) {
-        this.endStation = endStation;
+    public void setEndStationID(int endStationID) {
+        this.endStationID = endStationID;
     }
 
     public int getTicketsTotal() {
@@ -97,17 +115,20 @@ public class Departures {
         this.ticketsTotal = ticketsTotal;
     }
 
-    private int DepartureID;
+    private int departureID;
     private Date startDate;
     private Date endDate;
     private String length;
     private int ticketsLeft;
-    private TrainStations startStation;
-    private TrainStations endStation;
+    private int startStationID;
+    private int endStationID;
     private int ticketsTotal;
+
+    private String startTime;
+    private String endTime;
 
     @Override
     public int hashCode() {
-        return Objects.hash(DepartureID,startDate,endDate,length,ticketsLeft,ticketsTotal, startStation, endStation);
+        return Objects.hash(departureID,startDate,endDate,length,ticketsLeft,ticketsTotal, startStationID, endStationID);
     }
 }
