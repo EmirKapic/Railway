74527a20ed4ba9b019904ff10dbf2350c98916ec
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-11 18:27:50 +0100

    added error-checking for password/username fields

--- src/main/java/ba/unsa/etf/rpr/Login.java
+++ src/main/java/ba/unsa/etf/rpr/Login.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Login {
+
+    public PasswordField passwordField;
+    public TextField usernameField;
+
+    public void loginButtonClick(ActionEvent actionEvent) {
+        if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty()){
+            Alert emptyField = new Alert(Alert.AlertType.ERROR);
+            emptyField.setTitle("Greska");
+            emptyField.setHeaderText("Korisničko ime i lozinka ne mogu biti prazni! Unesite ponovo");
+            emptyField.showAndWait();
+        }
+    }
+
+
+
 }
