14f77ad5aba9cfe8a12729f3efa494cc0e43bf0b
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-11 18:33:48 +0100

    jaava fx fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module Railway {
     requires javafx.graphics;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.controls;
 
     requires org.controlsfx.controls;
 
     opens ba.unsa.etf.rpr to javafx.fxml;
     exports ba.unsa.etf.rpr;
-
 }
