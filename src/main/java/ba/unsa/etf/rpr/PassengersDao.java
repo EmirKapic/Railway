43e2a2fbd52d97f65953276cb30c4be53e9b0ff9
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-11 21:10:01 +0100

    added getting all tickets which belong to a person

--- src/main/java/ba/unsa/etf/rpr/PassengersDao.java
+++ src/main/java/ba/unsa/etf/rpr/PassengersDao.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface PassengersDao extends Dao<Passengers>{
     //public List<Passengers> searchByName(String name); SHOULD NOT BE USED! People can have the same name which can easily create problems. Use ID only
+    List<Tickets> getAllTickets(int PassengerID);
 }
