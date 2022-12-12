f8e7cf17363287b5a2a1a10d007547dab4fe6e2c
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-12 17:16:09 +0100

    added opening new window when user clicks on register

--- src/main/java/ba/unsa/etf/rpr/Login.java
+++ src/main/java/ba/unsa/etf/rpr/Login.java
@@ -1,12 +1,20 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login {
-
+    private Stage stage;
+    private Scene scene;
     public PasswordField passwordField;
     public TextField usernameField;
 
@@ -17,8 +25,13 @@ public class Login {
             emptyField.setHeaderText("Korisničko ime i lozinka ne mogu biti prazni! Unesite ponovo");
             emptyField.showAndWait();
         }
+
     }
-
-
-
+    public void registerButtonClick(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/registering.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
