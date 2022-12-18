d03dd34923a1e98667fb6936d9c1981efddcba4e
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-18 13:14:00 +0100

    added implementation for log-off button

--- src/main/java/ba/unsa/etf/rpr/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/MainWindowController.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,18 +13,33 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainWindowController {
+    private Stage stage;
+    private Scene scene;
+
     public Button ReservationButton;
     public Button DeparturesButton;
 
 
     public BorderPane mainWindowPane;
     public ImageView TrainPic;
+    public Button logOffButton;
 
     @FXML
     public void initialize(){
         Image image = new Image("train2.jpg");
         TrainPic.setImage(image);
+    }
+
+    public void logOffButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
