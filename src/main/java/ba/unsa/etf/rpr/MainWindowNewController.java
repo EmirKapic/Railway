58453f985e75fa551ba6769fefbf4f0332aeef41
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-18 20:46:10 +0100

    added button functionally for switching to search departure window

--- src/main/java/ba/unsa/etf/rpr/MainWindowNewController.java
+++ src/main/java/ba/unsa/etf/rpr/MainWindowNewController.java
@@ -1,13 +1,22 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainWindowNewController {
+    private Stage stage;
+    private Scene scene;
     public ImageView leftImage;
     public ImageView rightPic2;
     public ChoiceBox entryChoice;
@@ -24,9 +33,9 @@ public class MainWindowNewController {
 
 
     public void initialize(){
-        Image image = new Image("railwaytrain.png");
+        Image image = new Image("Pictures/railwaytrain.png");
         leftImage.setImage(image);
-        image = new Image("rightpic2.jpg");
+        image = new Image("Pictures/rightpic2.jpg");
         rightPic2.setImage(image);
 
         TrainStationsDAOSQLImpl tsql = new TrainStationsDAOSQLImpl();
@@ -36,5 +45,16 @@ public class MainWindowNewController {
 
         entryChoice.getItems().addAll(locations);
         endChoice.getItems().addAll(locations);
+    }
+
+    public void searchButtonClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLFiles/departureSearch.fxml"));
+        Parent root = loader.load();
+        TicketSearchController tctrl = loader.getController();
+        tctrl.setMainLabel("All tickets from " + entryChoice.getValue() + " to " + endChoice.getValue());
+        stage = (Stage)(((Node)actionEvent.getSource()).getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
