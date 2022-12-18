9a165241f789b845a9b294809a9485c591a17c05
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-18 20:45:22 +0100

    added method for setting correct start and end stations

---
+++ src/main/java/ba/unsa/etf/rpr/TicketSearchController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import javafx.scene.control.Label;
+
+public class TicketSearchController {
+    public Label mainLabel;
+
+    public void setMainLabel(String label){
+        mainLabel.setText(label);
+    }
+}
