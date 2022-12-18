d9acec8b446e4f9ff2e91884d357f0634c237453
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-18 17:32:34 +0100

    buy new ticket button now opens a new window for purchase

--- src/main/java/ba/unsa/etf/rpr/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/MainWindowController.java
@@ -43,4 +43,12 @@ public class MainWindowController {
         stage.setResizable(false);
         stage.show();
     }
+
+    public void buyTicketButtonPressed(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/buyNewTicketWindow.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
