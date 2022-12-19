d95198b0c4938e265022c0af27847fbc77725fcc
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-19 17:10:18 +0100

    new login screen look

--- src/main/java/ba/unsa/etf/rpr/Login.java
+++ src/main/java/ba/unsa/etf/rpr/Login.java
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -13,6 +14,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Login {
+    public Button loginButton;
     private Stage stage;
     private Scene scene;
     public PasswordField passwordField;
