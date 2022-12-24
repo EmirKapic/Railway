fa2913a5999b527775373fbe25e22c78c9460185
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-24 11:10:53 +0100

    implemented getByID

--- src/main/java/ba/unsa/etf/rpr/Dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/AbstractDao.java
@@ -3,7 +3,10 @@ package ba.unsa.etf.rpr.Dao;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public abstract class AbstractDao<T> implements Dao<T>{
@@ -31,5 +34,44 @@ public abstract class AbstractDao<T> implements Dao<T>{
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
+    public abstract T row2object(ResultSet rs);
+    public abstract Map<String, Object> object2row(T item);
 
+    @Override
+    public T getById(int id){
+        return executeQueryUnique("SELECT * FROM " + this.tableName + " WHERE ID = ?", new Object[]{id});
+    }
+    @Override
+    public T add(T item){
+        return null;
+    }
+    @Override
+    public T update(T item){
+        return null;
+    }
+    @Override
+    public void delete(int id){
+
+    }
+    @Override
+    public List<T> getAll(){
+        return null;
+    }
+
+
+    public List<T> executeQuery(String query, Object[] parameters){
+        return null;
+    }
+
+    public T executeQueryUnique(String query, Object[] parameters){
+        return null;
+    }
+
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row){
+        return null;
+    }
+
+    private String prepareUpdateParts(Map<String, Object> row){
+        return null;
+    }
 }
