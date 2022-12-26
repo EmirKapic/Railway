1f5acd9b6c70a7aaf9e971b5a22fbe0eff7f18be
EmirKapic <ekapic1@etf.unsa.ba>
2022-12-26 19:43:42 +0100

    now shows start date for each departure

--- src/main/java/ba/unsa/etf/rpr/Controllers/TicketSearchController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/TicketSearchController.java
@@ -61,21 +61,28 @@ public class TicketSearchController {
     @FXML
     public void initialize(){
         List<Departures> departuresList = DaoFactory.departuresDao().searchByStation(startLocationProperty().get());
-        List<String> times = new ArrayList<>();
+        List<String> startTimes = new ArrayList<>();
+        List<String> endTimes = new ArrayList<>();
+        List<String> lengthList = new ArrayList<>();
+        for (Departures d : departuresList){
+            startTimes.add(d.getStartTime().toString());
+            endTimes.add(d.getEndTime().toString());
+            lengthList.add(d.getLength());
+        }
         this.mainLabel.setText("All departures from " + startLocationProperty().get() + " to " + endLocationProperty().get());
         for (int i = 0; i < departuresList.size(); ++i){
             Label start = new Label(); Label end = new Label();
             start.setText(startLocationProperty().get()); end.setText(endLocationProperty().get());
             Label time = new Label();
-            time.setText("08:30");
+            time.setText(startTimes.get(i));
             time.getStyleClass().add("time");
 
             Label time2 = new Label();
-            time2.setText("15:00");
+            time2.setText(endTimes.get(i));
             time2.getStyleClass().add("time");
 
             Label length = new Label();
-            length.setText("2h50min");
+            length.setText(lengthList.get(i));
             length.getStyleClass().add("length");
 
             start.getStyleClass().add("city");
@@ -86,12 +93,16 @@ public class TicketSearchController {
             lenText.setText("Length");
             lenText.getStyleClass().add("lenText");
 
+            Label startDate = new Label();
+            startDate.setText(departuresList.get(i).getStartDate().toString());
+
             GridPane next = new GridPane();
             next.setHgap(90);
             next.getStyleClass().add("departure");
             next.add(time,0,0);
             next.add(length, 1, 0);
             next.add(time2, 2, 0);
+            next.add(startDate, 3, 0);
             next.add(start, 0, 1);
             next.add(lenText, 1, 1);
             next.add(end,2,1);
