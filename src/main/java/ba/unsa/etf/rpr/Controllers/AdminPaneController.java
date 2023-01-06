cfa9c15616354da72f6522bbceb2ba8421e740c0
EmirKapic <ekapic1@etf.unsa.ba>
2023-01-06 16:08:30 +0100

    implemented choicebox showing all cities

--- src/main/java/ba/unsa/etf/rpr/Controllers/AdminPaneController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/AdminPaneController.java
@@ -1,10 +1,16 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.Dao.DaoFactory;
+import ba.unsa.etf.rpr.Domain.TrainStations;
+import ba.unsa.etf.rpr.Exceptions.StatementException;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminPaneController {
 
@@ -24,10 +30,25 @@ public class AdminPaneController {
         warningMsg1.setVisible(false);
         warningMsg2.setVisible(false);
         warningMsg3.setVisible(false);
+        try {
+
+            startChoice.getItems().addAll(getAllLocations(DaoFactory.trainStationsDao().getAll()));
+            endChoice.getItems().addAll(getAllLocations(DaoFactory.trainStationsDao().getAll()));
+        } catch (StatementException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void depAddBtn(ActionEvent actionEvent) {
 
+    }
+
+
+    private List<String> getAllLocations(List<TrainStations> stat){
+        List<String> cities = new ArrayList<>();
+        for (TrainStations t : stat)
+            cities.add(t.getLocation());
+        return cities;
     }
 }
